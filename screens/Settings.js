2ee93ccb2576651f76cb301106a82b145155fcd8
Vinoth Raaj <vinoth@Vinoths-MacBook-Air.local>
2020-06-22 20:57:06 +0530

    adds version id

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,67 +1,81 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import VersionNumber from 'react-native-version-number';
 
 import Styles from '../constants/styles';
 import Colors from '../constants/colors';
 
 import Header from '../components/header';
 
-
 export default class Settings extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            telId: '',
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      telId: '',
+    };
+  }
 
-    componentDidMount() {
-        this.getTelegramId();
-    }
+  componentDidMount() {
+    this.getTelegramId();
+  }
 
-    getTelegramId() {
-        this.setState({ telId: '1234567890' })
-    }
+  getTelegramId() {
+    this.setState({telId: '1234567890'});
+  }
 
-    render() {
-        const { navigation } = this.props;
-        const { telId } = this.state;
-        return (
-            <>
-                <SafeAreaView style={{ backgroundColor: Colors.notificationBar }} />
-                <ScrollView
-                    style={{ flex: 1, backgroundColor: Colors.background, padding: 10 }}>
-                    <Header navigation={navigation} isBack={true} />
-                    <Text style={[Styles.headingText, { marginTop: 10 }]}>Settings</Text>
-                    <View style={{ flexDirection: 'row', marginTop: 10, }}>
-                        <TextInput placeholder="Enter Telegram ID" style={[Styles.inputBox, { width: '80%' }]} placeholderTextColor={'white'} onChangeText={v => this.setState({ telId: v })} value={telId} />
+  render() {
+    const {navigation} = this.props;
+    const {telId} = this.state;
+    return (
+      <>
+        <SafeAreaView style={{backgroundColor: Colors.notificationBar}} />
+        <ScrollView
+          style={{flex: 1, backgroundColor: Colors.background, padding: 10}}>
+          <Header navigation={navigation} isBack={true} />
+          <Text style={[Styles.headingText, {marginTop: 10}]}>Settings</Text>
+          <View style={{flexDirection: 'row', marginTop: 10}}>
+            <TextInput
+              placeholder="Enter Telegram ID"
+              style={[Styles.inputBox, {width: '80%'}]}
+              placeholderTextColor={'white'}
+              onChangeText={(v) => this.setState({telId: v})}
+              value={telId}
+            />
 
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.buttonText}>Done</Text>
-                        </TouchableOpacity>
-                    </View>
-
-                </ScrollView>
-                <SafeAreaView style={{ backgroundColor: Colors.background }} />
-            </>
-        );
-    }
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Done</Text>
+            </TouchableOpacity>
+          </View>
+          <Text>app v{VersionNumber.appVersion}</Text>
+        </ScrollView>
+        <SafeAreaView style={{backgroundColor: Colors.background}} />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    button: {
-        borderRadius: 10,
-        backgroundColor: Colors.orange,
-        height: 50,
-        alignSelf: 'center',
-        marginBottom: 10,
-        marginLeft: 10,
-        justifyContent: 'center',
-        flex: 1
-    },
-    buttonText: {
-        textAlign: 'center',
-        fontWeight: '700',
-        color: 'white'
-    }
+  button: {
+    borderRadius: 10,
+    backgroundColor: Colors.orange,
+    height: 50,
+    alignSelf: 'center',
+    marginBottom: 10,
+    marginLeft: 10,
+    justifyContent: 'center',
+    flex: 1,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: '700',
+    color: 'white',
+  },
 });
