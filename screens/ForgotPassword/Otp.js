248d91da01b7287406a464986bab8d6f5f2285b7
ciddarth <ciddarthjeyakumar@gmail.com>
2020-06-27 00:06:31 +0530

    Adds Otp, Phone page

---
+++ screens/ForgotPassword/Otp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+
+import Colors from '../../constants/colors';
+import Styles from '../../constants/styles';
+
+
+export default class Otp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        const { navigation } = this.props;
+        return (
+            <>
+                <SafeAreaView style={{ backgroundColor: Colors.notificationBar }} />
+                <ScrollView
+                    style={{ flex: 1, backgroundColor: Colors.background, padding: 10 }}>
+
+                </ScrollView>
+                <SafeAreaView style={{ backgroundColor: Colors.background }} />
+            </>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
