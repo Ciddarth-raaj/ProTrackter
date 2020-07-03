05c085d27dc259be71cdcd58d1b7fc3d3f87688b
ciddarth <ciddarthjeyakumar@gmail.com>
2020-07-03 12:50:06 +0530

    Adds FlatList in Tasks User

--- screens/User/Tasks.js
+++ screens/User/Tasks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import { SafeAreaView, ScrollView, Text, View, FlatList, RefreshControl } from 'react-native';
 
 import Colors from '../../constants/colors';
 import Styles from '../../constants/styles';
@@ -37,16 +37,17 @@ export default class Tasks extends React.Component {
       title: types[type].title,
       color: types[type].color,
       tasks: [],
+      refreshing: false
     };
   }
 
-  componentDidMount() {
+  getTasks() {
     const tag = this.props.route.params.tag;
     API.get(`/task/filter?${tag}=true`)
       .then((res) => {
         if (res.data.code === 200) {
           const tasks = util.formatResponse(res.data.tasks);
-          this.setState({tasks: tasks});
+          this.setState({ tasks: tasks, refreshing: false });
         } else {
           alert(res.data.msg);
         }
@@ -56,38 +57,62 @@ export default class Tasks extends React.Component {
       });
   }
 
-  render() {
-    const {title, color, tasks} = this.state;
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  renderCards(t) {
+    return (
+      <TaskCard
+        key={'task-' + t.id}
+        id={t.id}
+        title={t.title}
+        product={t.product}
+        assignee={t.assignee}
+        color={t.color}
+        description={t.description}
+        state={t.state}
+        status={t.status}
+      />
+    )
+  }
+
+  listHeader() {
+    const { title, color } = this.state;
     return (
       <>
-        <SafeAreaView style={{backgroundColor: Colors.notificationBar}} />
-        <ScrollView
-          style={{flex: 1, backgroundColor: Colors.background, padding: 10}}>
-          <Header isBack={true} navigation={this.props.navigation} />
+        <Header isBack={true} navigation={this.props.navigation} />
 
-          <Text style={[Styles.headingText, {color: color, marginTop: 10}]}>
-            {title}
-          </Text>
-          <Text style={[Styles.headingText, {marginBottom: 10}]}>
-            {'Today'}
-          </Text>
+        <Text style={[Styles.headingText, { color: color, marginTop: 10 }]}>
+          {title}
+        </Text>
+        <Text style={[Styles.headingText, { marginBottom: 10 }]}>
+          {'Today'}
+        </Text>
+      </>
+    )
+  }
 
-          <View style={Styles.tasksWrapper}>
-            {tasks.map((t) => (
-              <TaskCard
-                key={'task-' + t.id}
-                id={t.id}
-                title={t.title}
-                product={t.product}
-                assignee={t.assignee}
-                color={t.color}
-                description={t.description}
-                state={t.state}
-                status={t.status}
-              />
-            ))}
-          </View>
-        </ScrollView>
+  onRefresh = () => {
+    this.setState({ refreshing: true });
+    this.getTasks();
+  }
+
+  render() {
+    const { title, color, tasks, refreshing } = this.state;
+    return (
+      <>
+        <SafeAreaView style={{ backgroundColor: Colors.notificationBar }} />
+
+        <View style={{ backgroundColor: 'white', flex: 1 }}>
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => this.renderCards(item)}
+            keyExtractor={item => item.id}
+            refreshControl={<RefreshControl refreshing={refreshing} onRefresh={this.onRefresh} />}
+            contentContainerStyle={{ backgroundColor: Colors.background, padding: 10 }}
+            ListHeaderComponent={this.listHeader()} />
+        </View>
       </>
     );
   }
